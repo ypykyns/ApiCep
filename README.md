23037445d0721a4d30b6b45597570095a4804598
Luis Eduardo <ypykyns@gmail.com>
2020-05-18 16:12:03 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ApiCep
+
+Api construída para estudos dos conceitos de consumo de api, e git pages
---
+++ index.js
@@ -0,0 +1,31 @@
+//$(document).ready(function(){
+//
+//        alert("Página sem as funcções javascript implementadas")
+//});
+
+
+$(document).ready(function(){
+    $("#txtCep").focusout(function(){
+            var cep = $("#txtCep").val();
+            cep = cep.replace("-", "");
+            var urlStr = "https://viacep.com.br/ws/"+ cep +"/json/";
+         
+       
+            $.ajax({
+                url : urlStr,
+                type : "get",
+                dataType : "json",
+                success : function(data){                   
+                     
+                    $("#txtCidade").val(data.localidade);
+                    $("#txtEstado").val(data.uf);
+                    $("#txtBairro").val(data.bairro);
+                    $("#txtRua").val(data.logradouro);
+                    $("#txtComplemento").val(data.complemento);
+                },
+                error : function(erro){
+                    console.log(erro);
+                }
+            });
+    });
+});
